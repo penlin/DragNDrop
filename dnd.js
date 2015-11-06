daca20935bb08a67731c151b1277ec6052ad3eaa
LinKuan-yu <penlin@Penlin-2.local>
2015-11-06 14:09:20 +0800

    [M] draggable listitems between containers

--- dnd.js
+++ dnd.js
@@ -1,11 +1,11 @@
 var g_dragSrcEl = null;
-
+var g_dragFromCondition = null;
 function handleDragStart(e) {
-	this.style.opacity = '0.4';  // this / e.target is the source node.
+	// this / e.target is the source node.
 	g_dragSrcEl = this;
-
-	e.dataTransfer.effectAllowed = 'move';
-	e.dataTransfer.setData('text/html', this.innerHTML);
+	//e.dataTransfer.effectAllowed = 'move';
+	//e.dataTransfer.setData('text/html', this.outerHTML);
+	this.style.opacity = '0.4';  
 }
 
 function handleDragOver(e) {
@@ -34,31 +34,38 @@ function handleDrop(e) {
   }
   this.classList.remove('over');
   // See the section on the DataTransfer object.
-  if (g_dragSrcEl != this) {
-    // Set the source column's HTML to the HTML of the column we dropped on.
-    g_dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-	
+  g_dragSrcEl.style.opacity = '1';
+  var node = g_dragSrcEl.cloneNode(true);
+  node.addEventListener('dragstart',handleDragStart, false);
+  node.addEventListener('dragend',handleDragEnd, false);
+  this.getElementsByClassName('content-list')[0].appendChild(node);
+  g_dragSrcEl.parentNode.removeChild(g_dragSrcEl);
   return false;
 }
 
 function handleDragEnd(e) {
 	// this/e.target is the source node.
-	var cols = document.querySelectorAll('#columns .column');
-	this.style.opacity = '1';
-  	g_dragSrcEl = null;
+	if (g_dragSrcEl != null) {
+		g_dragSrcEl.style.opacity = '1';
+		g_dragSrcEl = null;
+	}
 }
 
 function onLoadDone() {
 	var cols = document.querySelectorAll('#columns .column');
 	console.log('document onload done ! #cols:' + cols.length);
 	[].forEach.call(cols, function(col) {
-	  	col.addEventListener('dragstart', handleDragStart, false);
+	  	//col.addEventListener('dragstart', handleDragStart, false);
 		col.addEventListener('dragenter', handleDragEnter, false);
 		col.addEventListener('dragover', handleDragOver, false);
 		col.addEventListener('dragleave', handleDragLeave, false);
 		col.addEventListener('drop', handleDrop, false);
-		col.addEventListener('dragend', handleDragEnd, false);
+		//col.addEventListener('dragend', handleDragEnd, false);
+	});
+
+	var items = document.querySelectorAll('p.listitem');
+	[].forEach.call(items, function(item){
+		item.addEventListener('dragstart',handleDragStart, false);
+		item.addEventListener('dragend',handleDragEnd, false);
 	});
 }
